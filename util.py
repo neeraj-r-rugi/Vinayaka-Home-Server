fbc68a1863e1b8879b0346f9bc4779b6237c22df
Neeraj R Rugi <texonace123@gmail.com>
2025-07-26 15:22:10 +0530

    Added Argument Parser and Updated CSS

---
+++ util.py
@@ -0,0 +1,17 @@
+import argparse
+
+def init_parser() ->argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(prog="VinayakaHomeServer", description="A Local WAN Home Data Server")
+    parser.add_argument("--data-dir", "-dr",
+                        default="/media/neeraj-r-rugi/NEERAJ_DRIVE",
+                        help="The Directory which the server is to host.")
+    parser.add_argument("--password", "-ps",
+                        default="Dingus",
+                        help="The Password for Logging into your server.")
+    parser.add_argument("--exclude-dir", "-ed",
+                        nargs="+",
+                        help="List The Directories to be Excluded.")
+    parser.add_argument("--debug", "-db",
+                        action="store_true",
+                        help="Enable Debug Mode for Flask")
+    return parser
